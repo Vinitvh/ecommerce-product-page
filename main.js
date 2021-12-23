0759599b2b055dbf4656d2da0d409874d6d7831c
Vinit Hemadri <vineet.vh@gmail.com>
2021-12-23 23:39:03 +0530

    Updated main.js

--- main.js
+++ main.js
@@ -3,4 +3,46 @@ const decrement = document.getElementById("decrement");
 const qtyDOM = document.querySelector(".qty");
 const cartDOM = document.querySelector(".cart-product");
 const checkoutBtn = document.querySelector(".checkout-btn");
-const cartSpan = document.querySelector(".cart-items");
+const cartItemsSpan = document.querySelector(".cart-items");
+
+const productInCart = {
+  id: 1234567,
+  title: "Fall Limited Edition Sneakers",
+  price: 125,
+  qty: 1,
+};
+
+const Storage = (function () {
+  const saveProduct = function () {
+    return localStorage.setItem("productInCart", JSON.stringify(productInCart));
+  };
+
+  const getProduct = function () {
+    JSON.parse(localStorage.getItem("productInCart"));
+  };
+
+  const saveCartNumber = function () {
+    let cartNumber = productInCart.qty;
+    localStorage.setItem("cartNumber", cartNumber);
+  };
+
+  const getCartNumber = function () {
+    JSON.parse(localStorage.getItem("cartNumber"));
+  };
+
+  return { saveProduct, getProduct, saveCartNumber, getCartNumber };
+})();
+
+const App = (function () {
+  const manageQty = function () {
+    increment.addEventListener("click", () => {
+      let qty = Storage.getProduct().qty;
+      qty = parseInt(qty);
+
+      qtyDOM.innerText += 1;
+    });
+  };
+})();
+
+Storage.saveProduct();
+Storage.saveCartNumber();
