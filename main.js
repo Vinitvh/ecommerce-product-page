64a4b018e5d45836d9aa21cd739c3686b60da149
Vinit Hemadri <vineet.vh@gmail.com>
2021-12-24 00:08:05 +0530

    Rebuilt shopping cart

--- main.js
+++ main.js
@@ -12,37 +12,34 @@ const productInCart = {
   qty: 1,
 };
 
-const Storage = (function () {
-  const saveProduct = function () {
-    return localStorage.setItem("productInCart", JSON.stringify(productInCart));
-  };
-
-  const getProduct = function () {
-    JSON.parse(localStorage.getItem("productInCart"));
-  };
-
-  const saveCartNumber = function () {
-    let cartNumber = productInCart.qty;
-    localStorage.setItem("cartNumber", cartNumber);
-  };
-
-  const getCartNumber = function () {
-    JSON.parse(localStorage.getItem("cartNumber"));
-  };
-
-  return { saveProduct, getProduct, saveCartNumber, getCartNumber };
-})();
-
 const App = (function () {
   const manageQty = function () {
     increment.addEventListener("click", () => {
-      let qty = Storage.getProduct().qty;
-      qty = parseInt(qty);
+      let qty = parseInt(qtyDOM.innerText);
+      qtyDOM.innerText = qty + 1;
+      localStorage.setItem("cartNumber", JSON.stringify(qty + 1));
+      setCartNumber();
+    });
 
-      qtyDOM.innerText += 1;
+    decrement.addEventListener("click", () => {
+      let qty = parseInt(qtyDOM.innerText);
+      if (qty <= 0) {
+        decrement.click = false;
+      } else {
+        qtyDOM.innerText = qty - 1;
+        localStorage.setItem("cartNumber", JSON.stringify(qty - 1));
+        setCartNumber();
+      }
     });
   };
+
+  const setCartNumber = function () {
+    let cartNumber = JSON.parse(localStorage.getItem("cartNumber"));
+    cartItemsSpan.innerText = cartNumber;
+  };
+
+  return { manageQty, setCartNumber };
 })();
 
-Storage.saveProduct();
-Storage.saveCartNumber();
+App.manageQty();
+App.setCartNumber();
